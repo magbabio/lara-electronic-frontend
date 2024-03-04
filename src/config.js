7607c6226896d5b274f01f9d04b69af07a2933b7
Mariel García <magbabio@gmail.com>
2024-03-04 12:45:14 -0400

    Cambios

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL='http://localhost:4000/api';
-export const HOST='192.168.1.107';
+export const HOST='192.168.1.108';
 export const PORT=3030;
--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
     ],
   },
   server: {
-    host: '192.168.1.107',
+    host: '192.168.1.108',
     port: 3030,
   },
   preview: {
